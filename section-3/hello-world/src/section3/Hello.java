3ac5e022770bf45d5ba997ac821b111c7860adb2
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-22 21:54:02 -0300

    Variables

--- section-3/hello-world/src/section3/Hello.java
+++ section-3/hello-world/src/section3/Hello.java
@@ -2,6 +2,12 @@ package section3;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.print("Hello, Gabriel");
+		System.out.println("Hello, Gabriel");
+		
+		int number = 7;
+		int number2 = (7 + 7) * (7 + 7);
+		
+		System.out.println(number);
+		System.out.println(number2);
 	}
 }
