bf611f85888622923096bde20329b7648a32c485
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-22 22:17:17 -0300

    Starting out with Expressions

--- section-3/hello-world/src/section3/Hello.java
+++ section-3/hello-world/src/section3/Hello.java
@@ -4,10 +4,12 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, Gabriel");
 		
-		int number = 7;
-		int number2 = (7 + 7) * (7 + 7);
+		int number = 12;
+		int number2 = (number + 7) * (7 + number);
+		int total = number + number2;
 		
 		System.out.println(number);
 		System.out.println(number2);
+		System.out.println(total);
 	}
 }
