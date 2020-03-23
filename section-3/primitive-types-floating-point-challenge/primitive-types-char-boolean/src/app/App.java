cc5f1e58673fdd30999dc84166bf8276a6c4fa48
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-23 20:34:28 -0300

    The char and boolean Primitive Data Types

---
+++ section-3/primitive-types-floating-point-challenge/primitive-types-char-boolean/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        char myChar = 'a';
+        char myUnicode = '\u0B85';
+
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+
+        boolean myTrue = true;
+        boolean myFalse = false;
+
+        System.out.println(myTrue);
+        System.out.println(myFalse);
+    }
+}
