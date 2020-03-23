4add6475a8b8b13fa9210961c5d837056652a944
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-23 12:00:27 -0300

    Section 3: byte, short, long and width

--- section-3/primitive-types/src/app/App.java
+++ section-3/primitive-types/src/app/App.java
@@ -1,16 +1,49 @@
 package app;
 
 public class App {
-    public static void main(final String[] args) throws Exception {
-        final int minValue = Integer.MIN_VALUE;
-        final int maxValue = Integer.MAX_VALUE;
+    public static void main(String[] args) throws Exception {
+        System.out.println("-------");
+        System.out.println("Integer");
+        System.out.println("-------");
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
 
         System.out.println("Min value: " + minValue);
         System.out.println("Max value: " + maxValue);
-        System.out.println("Max value busted: " + (maxValue + 1));
-        System.out.println("Min value busted: " + (minValue - 1));
+        // System.out.println("Max value busted: " + (maxValue + 1));
+        // System.out.println("Min value busted: " + (minValue - 1));
         
-        int maxValue2 = 2_147_483_647;
-        System.out.println("Max value 2 busted: " + (maxValue2 + 1));
+        // int maxValue2 = 2_147_483_647;
+        // System.out.println("Max value 2 busted: " + (maxValue2 + 1));
+
+        // ------ //
+        System.out.println("----");
+        System.out.println("Byte");
+        System.out.println("----");
+        byte minByteValue = Byte.MIN_VALUE;
+        byte maxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Min byte value: " + minByteValue);
+        System.out.println("Max byte value: " + maxByteValue);
+
+        // ------ //
+        System.out.println("----");
+        System.out.println("Short");
+        System.out.println("----");
+        short minShortValue = Short.MIN_VALUE;
+        short maxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Min byte value: " + minShortValue);
+        System.out.println("Max byte value: " + maxShortValue);
+
+        // ------ //
+        System.out.println("----");
+        System.out.println("Long");
+        System.out.println("----");
+        long minLongValue = Long.MIN_VALUE;
+        long maxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Min long value: " + minLongValue);
+        System.out.println("Max long value: " + maxLongValue);
     }
 }
