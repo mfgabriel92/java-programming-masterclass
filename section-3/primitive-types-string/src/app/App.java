139ce80b06aad64b6e45933c120f5fb56b54d384
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-23 20:53:47 -0300

    Primitive Types Recap and the String Data Type

--- section-3/primitive-types-string/src/app/App.java
+++ section-3/primitive-types-string/src/app/App.java
@@ -2,6 +2,18 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        String myString = "This is my string";
+        System.out.println("My string is: " + myString);
+
+        myString = myString + ", and this too.";
+        System.out.println("My string is: " + myString);
+
+        String myNumberString = "300.50";
+        myNumberString = myNumberString + "500.45";
+        System.out.println(myNumberString);
+
+        int myInt = 10;
+        myNumberString = "300.50" + myInt;
+        System.out.println(myNumberString);
     }
 }
