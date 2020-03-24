d6e8c16023af1a01577f024e00b51c78dc4e4ef7
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-24 10:31:15 -0300

    if-then Statement

---
+++ section-3/operators-operands-expressions-if-then/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean isAlien = false;
+
+        if (isAlien == false) {
+            System.out.println("Is not alien");
+        }
+    }
+}
