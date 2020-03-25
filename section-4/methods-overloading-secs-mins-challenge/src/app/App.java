5a1516c3eb4d7db8f2a4eb07f9152d11f9c7bf92
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-25 13:25:53 -0300

    Bonus Challenge Solution

--- section-4/methods-overloading-secs-mins-challenge/src/app/App.java
+++ section-4/methods-overloading-secs-mins-challenge/src/app/App.java
@@ -1,26 +1,36 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(getDurationString(4000));
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
 
+    public static void main(String[] args) throws Exception {
+        System.out.println(getDurationString(4021));
     }
 
     public static String getDurationString(int minutes, int seconds) {
         if ((minutes < 0) || ((seconds < 0 || seconds > 60))) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int hours = minutes / 60;
         minutes = (minutes % 60) + (seconds / 60);
         seconds = seconds % 60;
 
-        return hours + "h " + minutes + "m " + seconds + "s";
+        String hoursString = Integer.toString(hours);
+        hoursString = hours < 10 ? "0" + hours : hoursString;
+
+        String minutesString = Integer.toString(minutes);
+        minutesString = minutes < 10 ? "0" + minutes : minutesString;
+
+        String secondsString = Integer.toString(seconds);
+        secondsString = seconds < 10 ? "0" + seconds : secondsString;
+
+        return hoursString + "h " + minutesString + "m " + secondsString + "s";
     }
 
     public static String getDurationString(int seconds) {
         if (seconds < 0) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int minutes = seconds / 60;
