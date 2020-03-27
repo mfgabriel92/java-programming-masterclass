9f72bc9d6671295a1675ec5df44951e88ba8eba7
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-03-27 14:56:43 -0300

    Classes Part 2

--- section-6/oop-classes/src/app/App.java
+++ section-6/oop-classes/src/app/App.java
@@ -3,5 +3,8 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         Car yaris = new Car();
+
+        yaris.setModel("Ford");
+        System.out.println(yaris.getModel());
     }
 }
--- section-6/oop-classes/src/app/Car.java
+++ section-6/oop-classes/src/app/Car.java
@@ -4,4 +4,35 @@ public class Car {
     private int doors;
     private String model;
     private String color;
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        String m = model.toLowerCase();
+
+        if (m.equals("yaris")) {
+            this.model = model;
+        } else {
+            this.model = "Unkonwn";
+        }
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
